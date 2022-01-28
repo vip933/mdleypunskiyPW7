aab01d679c8688eb5889889ce962dfe6d84e9f19
vip933 <mdleypunskiy@edu.hse.ru>
2022-01-28 18:59:19 +0300

    YMK API key now stored properly in singleton class

---
+++ mdleypunskiyPW7/YandexMapKitAPIKey.swift
@@ -0,0 +1,17 @@
+//
+//  YandexMapKitAPIKey.swift
+//  mdleypunskiyPW7
+//
+//  Created by Maksim on 28.01.2022.
+//
+
+final class YMKAPIKey {
+    
+    private static let _key = "aa9ef67a-24a9-4fd0-b4a3-2ccee32783cd"
+    
+    public static var getKey: String {
+        get {
+            return _key
+        }
+    }
+}
